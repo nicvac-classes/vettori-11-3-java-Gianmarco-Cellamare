2913f1bb679ea4f2af651431358512fe7c365d19
Gianmarco-Cellamare <gianmarco.cellamare@marconibari.edu.it>
2025-01-22 18:03:54 +0000

    Vettori 11.3

--- Esercizio.java
+++ Esercizio.java
@@ -1,26 +1,69 @@
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
 
-//Import di Classi Java necessarie al funzionamento del programma
 import java.util.Scanner;
 
-// Classe principale, con metodo main
-class Esercizio {
-    // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+public int leggicomando(){
+    String c;
+    do {
+        System.out.println("[I] Inserisci elemento | [E] Elimina elemento | [R] Ricerca elemento | [D] Elimina duplicati | [V] sualizza vettore | [S] Esci");
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
-
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
-
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
-    }
+    } while(c!="I" && c!="E" && c!="R" && c!="D" && c!="V" && c!="S");
+    return c;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+public int RicercaElemento (int [] V = int [] , int valore){
+    int R;
+    return R;
+}
+
+public int InserisciElemento (int [] V = int [] , int valore){
+    int I;
+    return I;
+}
+
+public int EliminaElemento (int [] V = int [] , int valore){
+    int E;
+    return E;
+}
+
+
+public int ricercaNelVettore (int [] V = int [] , int valore){
+    int i;
+    return i;
+}
+
+class Esercizio {
+
+    public static void main(String args[])
+    {
+        Scanner in = new Scanner( System.in );
+        int N, valore, I, E, R, D;
+        String c;
+        N = in.nextLine();
+        int [] V = int [N];
+        do {
+            c=leggicomando();
+
+            if (c = "I"){
+                I=InserisciElemento(V,valore);
+            }
+
+            if (c = "E"){
+                E=EliminaElemento(V,valore);
+            }
+
+            if (c = "R"){
+                R=RicercaElemento(V,valore);
+            }
+
+            if (c = "D"){
+                D=EliminaDuplicati(V,valore);
+            }
+
+            if (c = "V"){
+                i=ricercaNelVettore(V,valore);
+            }
+
+        } while (c!= "S");
+ 
+    }
+}
